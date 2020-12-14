a854a11437d25ab8d619727f30f4bbb7dd045b3a
tomislav <tomislav989s@gmail.com>
2020-12-14 13:30:41 +0100

    Init commit

---
+++ src/main/java/nonamebackend/noname/NonameApplication.java
@@ -0,0 +1,13 @@
+package nonamebackend.noname;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NonameApplication.class, args);
+    }
+
+}
