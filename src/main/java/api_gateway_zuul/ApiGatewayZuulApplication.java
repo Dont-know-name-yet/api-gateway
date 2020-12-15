ca76f5b143127ca63f2e692155c0505094dc8e69
tomislav <tomislav989s@gmail.com>
2020-12-15 18:44:46 +0100

    Eureka data updated

--- src/main/java/api_gateway_zuul/ApiGatewayZuulApplication.java
+++ src/main/java/api_gateway_zuul/ApiGatewayZuulApplication.java
@@ -1,4 +1,4 @@
-package apiGatewayZuul;
+package api_gateway_zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/api_gateway_zuul/controller/TestController.java
+++ src/main/java/api_gateway_zuul/controller/TestController.java
@@ -1,4 +1,4 @@
-package apiGatewayZuul.controller;
+package api_gateway_zuul.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
